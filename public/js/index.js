b4bc1a7173d97c237179bda77455977a3d38f961
robinchoii <robinchoii@gmail.com>
2016-07-13 16:48:30 -0700

    add scrolling, project zoom

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,41 @@
 $(document).ready(function () {
+  $(".projects").mouseover(function() {
+    $(this).css("opacity", ".5")
+  });
+  $(".projects").mouseout(function() {
+    $(this).css("opacity", "1")
+  });
+
+  $('img.img-thumbnail')
+  .wrap('<span style="display:inline-block"></span>')
+  .css('display', 'block')
+  .parent()
+  .zoom();
+
+  $('#submit').click(function() {
+    alert("Thank you for connecting!")
+  });
+
+  $("#nav-portfolio").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#portfolio").offset().top},
+      'slow');
+  });
+  $("#nav-about").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#about").offset().top},
+      'slow');
+  });
+  $("#nav-connect").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#connect").offset().top},
+      'slow');
+  });
+  $("#nav-top").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#page-top").offset().top},
+      'slow');
+  });
+
 
 });
